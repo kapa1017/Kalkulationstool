f51ada52d6481542cb9d97691dfe79dfd4390603
Pascal Kanzleiter <pascal.kanzleiter@live.de>
2017-11-19 12:07:54 +0100

    2 Schritt in Haupt-Applikation eingebunden

--- Kalkultationstool/src/app/app.component.ts
+++ Kalkultationstool/src/app/app.component.ts
@@ -1,28 +1,43 @@
 import { Component } from '@angular/core';
 import {StartService} from './Services/start.service';
 import 'rxjs/add/operator/map';
+import {StepTwoService} from "./Services/stepTwo.service";
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [StartService]
+  providers: [StartService, StepTwoService]
 })
 export class AppComponent {
   isStarted = false;
+  isStepTwo = false;
+  isStepThree = false;
+
   data = {};
   result: any;
 
-  constructor(private startService: StartService) {
+  constructor(private startService: StartService,
+              private stepTwoService: StepTwoService) {
     startService.isStarted$.subscribe((isStarted: boolean) => {
       this.isStarted = isStarted;
     });
+    stepTwoService.isStepTwo$.subscribe((isStepTwo: boolean) => {
+      this.isStepTwo = isStepTwo;
+    })
   }
 
   startCalculation() {
     if (this.isStarted === false) {
       this.startService.isStartedChanged(true);
+    }
+  }
+
+  gotoStepTwo(){
+    if(this.isStepTwo === false) {
+      this.stepTwoService.isStepTwoChanged(true);
+      this.startService.isStartedChanged(false);
     }
   }
 

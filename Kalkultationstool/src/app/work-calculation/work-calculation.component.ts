efd7e0cd891d59ae824726ce71f17df49eb81950
Pascal Kanzleiter <pascal.kanzleiter@live.de>
2017-11-19 21:10:53 +0100

    Schritt 4 Arbeitsplanung implementiert

--- Kalkultationstool/src/app/work-calculation/work-calculation.component.ts
+++ Kalkultationstool/src/app/work-calculation/work-calculation.component.ts
@@ -12,6 +12,38 @@ export class WorkCalculationComponent {
   auftraegeP2 = {};
   auftraegeP3 = {};
 
+  weekMinutes = 2400;
+
+  worktime1 = 0;
+  worktime2 = 0;
+  worktime3 = 0;
+  worktime4 = 0;
+  worktime5 = 0;
+  worktime6 = 0;
+  worktime7 = 0;
+  worktime8 = 0;
+  worktime9 = 0;
+  worktime10 = 0;
+  worktime11 = 0;
+  worktime12 = 0;
+  worktime13 = 0;
+  worktime14 = 0;
+
+  setupTime1 = 0;
+  setupTime2 = 0;
+  setupTime3 = 0;
+  setupTime4 = 0;
+  setupTime5 = 0;
+  setupTime6 = 0;
+  setupTime7 = 0;
+  setupTime8 = 0;
+  setupTime9 = 0;
+  setupTime10 = 0;
+  setupTime11 = 0;
+  setupTime12 = 0;
+  setupTime13 = 0;
+  setupTime14 = 0;
+
 
   constructor(private auftraegeService: AuftraegeService) {
     auftraegeService.auftraeggeP1$.subscribe((newState: Object) => {
@@ -23,6 +55,5 @@ export class WorkCalculationComponent {
     auftraegeService.auftraeggeP3$.subscribe((newState: Object) => {
       this.auftraegeP3 = newState
     });
-    debugger;
   }
 }

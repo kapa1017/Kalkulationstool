67e728273782c62409a2cbe58cdffbfcc5e15c0a
PaKanzleiter <pascal.kanzleiter@abilis.de>
2017-11-30 13:22:15 +0100

    Kalkulation der Schichten eingebaut

--- Kalkultationstool/src/app/work-calculation/work-calculation.component.ts
+++ Kalkultationstool/src/app/work-calculation/work-calculation.component.ts
@@ -87,40 +87,40 @@ export class WorkCalculationComponent {
 
     //Kalkulation der Arbeitszeiten
 
-    this.worktime1 = this.auftraegeP1.E49*6 + this.auftraegeP2.E54*6 + this.auftraegeP3.E29*6;
-    this.worktime2 = this.auftraegeP1.E50*5 + this.auftraegeP2.E55*5 + this.auftraegeP3.E30*5;
-    this.worktime3 = this.auftraegeP1.E51*5 + this.auftraegeP2.E56*6 + this.auftraegeP3.E31*6;
-    this.worktime4 = this.auftraegeP1.P1*6 + this.auftraegeP2.P2*7 + this.auftraegeP3.P3*7;
-    this.worktime5 = this.auftraegeP1.E16*2 + this.auftraegeP2.E16*2 + this.auftraegeP3.E16*2
-                    + this.auftraegeP1.E18*3 + this.auftraegeP2.E19*3 + this.auftraegeP3.E20*3;
-    this.worktime6 = this.auftraegeP1.E10*2 + this.auftraegeP2.E11*2 + this.auftraegeP3.E12*2
-                    + this.auftraegeP1.E13*2 + this.auftraegeP2.E14*2 + this.auftraegeP3.E15*2
-                    + this.auftraegeP1.E18*2 + this.auftraegeP2.E19*2 + this.auftraegeP3.E20*2
-                    + this.auftraegeP1.E26*2 + this.auftraegeP2.E26*2 + this.auftraegeP3.E26*2;
-    this.worktime7 = this.auftraegeP1.E10*1 + this.auftraegeP2.E11*2 + this.auftraegeP3.E12*2
-                    + this.auftraegeP1.E13*1 + this.auftraegeP2.E14*2 + this.auftraegeP3.E15*2
-                    + this.auftraegeP1.E18*3 + this.auftraegeP2.E19*3 + this.auftraegeP3.E20*3;
-    this.worktime8 = this.auftraegeP1.E10*3 + this.auftraegeP2.E11*3 + this.auftraegeP3.E12*3
-                    + this.auftraegeP1.E13*3 + this.auftraegeP2.E14*3 + this.auftraegeP3.E15*3
-                    + this.auftraegeP1.E18*2 + this.auftraegeP2.E19*2 + this.auftraegeP3.E20*2;
-    this.worktime9 = this.auftraegeP1.E4*4 + this.auftraegeP2.E5*4 + this.auftraegeP3.E6*4
-                    + this.auftraegeP1.E7*4 + this.auftraegeP2.E8*4 + this.auftraegeP3.E9*4;
-    this.worktime10 = this.auftraegeP1.E4*3 + this.auftraegeP2.E5*3 + this.auftraegeP3.E6*3
-                    + this.auftraegeP1.E7*3 + this.auftraegeP2.E8*3 + this.auftraegeP3.E9*3;
-    this.worktime11 = this.auftraegeP1.E10*3 + this.auftraegeP2.E11*3 + this.auftraegeP3.E12*3
-                    + this.auftraegeP1.E13*3 + this.auftraegeP2.E14*3 + this.auftraegeP3.E15*3;
-    this.worktime12 = this.auftraegeP1.E10*2 + this.auftraegeP2.E11*2 + this.auftraegeP3.E12*2
-                    + this.auftraegeP1.E13*2 + this.auftraegeP2.E14*2 + this.auftraegeP3.E15*2;
-    this.worktime13 = this. auftraegeP1.E16*3 + this.auftraegeP2.E16*3 + this.auftraegeP3.E16*3;
-    this.worktime14 = this.auftraegeP1.E17*3 + this.auftraegeP2.E17*3 + this.auftraegeP3.E17*3
-                    + this.auftraegeP1.E26*3 + this.auftraegeP2.E26*3 + this.auftraegeP3.E26*3;
+    this.worktime1 = this.auftraegeP1.E49 * 6 + this.auftraegeP2.E54 * 6 + this.auftraegeP3.E29 * 6;
+    this.worktime2 = this.auftraegeP1.E50 * 5 + this.auftraegeP2.E55 * 5 + this.auftraegeP3.E30 * 5;
+    this.worktime3 = this.auftraegeP1.E51 * 5 + this.auftraegeP2.E56 * 6 + this.auftraegeP3.E31 * 6;
+    this.worktime4 = this.auftraegeP1.P1 * 6 + this.auftraegeP2.P2 * 7 + this.auftraegeP3.P3 * 7;
+    this.worktime5 = this.auftraegeP1.E16 * 2 + this.auftraegeP2.E16 * 2 + this.auftraegeP3.E16 * 2
+      + this.auftraegeP1.E18 * 3 + this.auftraegeP2.E19 * 3 + this.auftraegeP3.E20 * 3;
+    this.worktime6 = this.auftraegeP1.E10 * 2 + this.auftraegeP2.E11 * 2 + this.auftraegeP3.E12 * 2
+      + this.auftraegeP1.E13 * 2 + this.auftraegeP2.E14 * 2 + this.auftraegeP3.E15 * 2
+      + this.auftraegeP1.E18 * 2 + this.auftraegeP2.E19 * 2 + this.auftraegeP3.E20 * 2
+      + this.auftraegeP1.E26 * 2 + this.auftraegeP2.E26 * 2 + this.auftraegeP3.E26 * 2;
+    this.worktime7 = this.auftraegeP1.E10 * 1 + this.auftraegeP2.E11 * 2 + this.auftraegeP3.E12 * 2
+      + this.auftraegeP1.E13 * 1 + this.auftraegeP2.E14 * 2 + this.auftraegeP3.E15 * 2
+      + this.auftraegeP1.E18 * 3 + this.auftraegeP2.E19 * 3 + this.auftraegeP3.E20 * 3;
+    this.worktime8 = this.auftraegeP1.E10 * 3 + this.auftraegeP2.E11 * 3 + this.auftraegeP3.E12 * 3
+      + this.auftraegeP1.E13 * 3 + this.auftraegeP2.E14 * 3 + this.auftraegeP3.E15 * 3
+      + this.auftraegeP1.E18 * 2 + this.auftraegeP2.E19 * 2 + this.auftraegeP3.E20 * 2;
+    this.worktime9 = this.auftraegeP1.E4 * 4 + this.auftraegeP2.E5 * 4 + this.auftraegeP3.E6 * 4
+      + this.auftraegeP1.E7 * 4 + this.auftraegeP2.E8 * 4 + this.auftraegeP3.E9 * 4;
+    this.worktime10 = this.auftraegeP1.E4 * 3 + this.auftraegeP2.E5 * 3 + this.auftraegeP3.E6 * 3
+      + this.auftraegeP1.E7 * 3 + this.auftraegeP2.E8 * 3 + this.auftraegeP3.E9 * 3;
+    this.worktime11 = this.auftraegeP1.E10 * 3 + this.auftraegeP2.E11 * 3 + this.auftraegeP3.E12 * 3
+      + this.auftraegeP1.E13 * 3 + this.auftraegeP2.E14 * 3 + this.auftraegeP3.E15 * 3;
+    this.worktime12 = this.auftraegeP1.E10 * 2 + this.auftraegeP2.E11 * 2 + this.auftraegeP3.E12 * 2
+      + this.auftraegeP1.E13 * 2 + this.auftraegeP2.E14 * 2 + this.auftraegeP3.E15 * 2;
+    this.worktime13 = this.auftraegeP1.E16 * 3 + this.auftraegeP2.E16 * 3 + this.auftraegeP3.E16 * 3;
+    this.worktime14 = this.auftraegeP1.E17 * 3 + this.auftraegeP2.E17 * 3 + this.auftraegeP3.E17 * 3
+      + this.auftraegeP1.E26 * 3 + this.auftraegeP2.E26 * 3 + this.auftraegeP3.E26 * 3;
 
     // Kalkulation der Setupzeiten
 
     var blackList = [];
 
     this.auftraegeGesamt.forEach(el => {
-      if(parseInt(el.quantity) > 0 && blackList.indexOf(el) < 0) {
+      if (parseInt(el.quantity) > 0 && blackList.indexOf(el) < 0) {
         blackList.push(el);
         switch (el.item) {
           case '1':
@@ -245,5 +245,35 @@ export class WorkCalculationComponent {
       }
     });
 
+    //Kalkulation der Schichten
+
+    this.shift1 = ((this.setupTime1+this.worktime1)/this.weekMinutes > 2.5) ? 3 :
+      ((this.setupTime1 + this.worktime1 / this.weekMinutes > 1.5) ? 2 : 1);
+    this.shift2 = ((this.setupTime2+this.worktime2)/this.weekMinutes > 2.5) ? 3 :
+      ((this.setupTime2 + this.worktime2 / this.weekMinutes > 1.5) ? 2 : 1);
+    this.shift3 = ((this.setupTime3+this.worktime3)/this.weekMinutes > 2.5) ? 3 :
+      ((this.setupTime3 + this.worktime3 / this.weekMinutes > 1.5) ? 2 : 1);
+    this.shift4 = ((this.setupTime4+this.worktime4)/this.weekMinutes > 2.5) ? 3 :
+      ((this.setupTime4 + this.worktime4 / this.weekMinutes > 1.5) ? 2 : 1);
+    this.shift5 = ((this.setupTime5+this.worktime5)/this.weekMinutes > 2.5) ? 3 :
+      ((this.setupTime5 + this.worktime5 / this.weekMinutes > 1.5) ? 2 : 1);
+    this.shift6 = ((this.setupTime6+this.worktime6)/this.weekMinutes > 2.5) ? 3 :
+      ((this.setupTime6 + this.worktime6 / this.weekMinutes > 1.5) ? 2 : 1);
+    this.shift7 = ((this.setupTime7+this.worktime7)/this.weekMinutes > 2.5) ? 3 :
+      ((this.setupTime7 + this.worktime7 / this.weekMinutes > 1.5) ? 2 : 1);
+    this.shift8 = ((this.setupTime8+this.worktime8)/this.weekMinutes > 2.5) ? 3 :
+      ((this.setupTime8 + this.worktime8 / this.weekMinutes > 1.5) ? 2 : 1);
+    this.shift9 = ((this.setupTime9+this.worktime9)/this.weekMinutes > 2.5) ? 3 :
+      ((this.setupTime9 + this.worktime9 / this.weekMinutes > 1.5) ? 2 : 1);
+    this.shift10 = ((this.setupTime10+this.worktime10)/this.weekMinutes > 2.5) ? 3 :
+      ((this.setupTime10 + this.worktime10 / this.weekMinutes > 1.5) ? 2 : 1);
+    this.shift11 = ((this.setupTime11+this.worktime11)/this.weekMinutes > 2.5) ? 3 :
+      ((this.setupTime11 + this.worktime11 / this.weekMinutes > 1.5) ? 2 : 1);
+    this.shift12 = ((this.setupTime12+this.worktime12)/this.weekMinutes > 2.5) ? 3 :
+      ((this.setupTime12 + this.worktime12 / this.weekMinutes > 1.5) ? 2 : 1);
+    this.shift13 = ((this.setupTime13+this.worktime13)/this.weekMinutes > 2.5) ? 3 :
+      ((this.setupTime13 + this.worktime13 / this.weekMinutes > 1.5) ? 2 : 1);
+    this.shift14 = ((this.setupTime14+this.worktime14)/this.weekMinutes > 2.5) ? 3 :
+      ((this.setupTime14 + this.worktime14 / this.weekMinutes > 1.5) ? 2 : 1);
   }
 }

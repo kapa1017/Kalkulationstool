28c2d2f94d960291ce73bd45d2fe1fd50ebfa1e6
PaKanzleiter <pascal.kanzleiter@abilis.de>
2017-12-10 14:14:17 +0100

    KPI Parser

--- Kalkultationstool/src/app/kpi/kpi.component.ts
+++ Kalkultationstool/src/app/kpi/kpi.component.ts
@@ -1,15 +1,71 @@
 import { Component, OnInit } from '@angular/core';
+import {BackendService} from "../Services/backend.service";
 
 @Component({
   selector: 'app-kpi',
   templateUrl: './kpi.component.html',
   styleUrls: ['./kpi.component.css']
 })
-export class KpiComponent implements OnInit {
+export class KpiComponent {
 
-  constructor() { }
+  JSONData: any;
+  result: any;
 
-  ngOnInit() {
+  productiveTimeAc: number;
+  productiveTimeAv: number;
+
+  efficiencyAc: number;
+  efficiencyAv: number;
+
+  idleTimeAc: number;
+  idleTimeAv: number;
+
+  idleTimeCostsAc: number;
+  idleTimeCostsAv: number;
+
+  storeValueAc: number;
+  storeValueAv: number;
+
+  storageCostsAc: number;
+  storageCostsAv: number;
+
+  profitAc: number;
+  profitAv: number;
+  profitAll: number;
+
+  constructor(backendService: BackendService) {
+    backendService.getData().subscribe((data: any) => {
+      this.JSONData = data;
+      this.updateValues();
+    });
   }
 
+  updateValues(){
+
+    debugger;
+
+    this.productiveTimeAc = this.JSONData.results.result[0].general[0].productivetime[0].$.current;
+    this.productiveTimeAv = this.JSONData.results.result[0].general[0].productivetime[0].$.average;
+
+    this.efficiencyAc = this.JSONData.results.result[0].general[0].effiency[0].$.current;
+    this.efficiencyAv = this.JSONData.results.result[0].general[0].effiency[0].$.average;
+
+    this.idleTimeAc = this.JSONData.results.result[0].general[0].idletime[0].$.current;
+    this.idleTimeAv = this.JSONData.results.result[0].general[0].idletime[0].$.average;
+
+    this.idleTimeCostsAc = this.JSONData.results.result[0].general[0].idletimecosts[0].$.current;
+    this.idleTimeCostsAv = this.JSONData.results.result[0].general[0].idletimecosts[0].$.average;
+
+    this.storeValueAc = this.JSONData.results.result[0].general[0].storevalue[0].$.current;
+    this.storeValueAv = this.JSONData.results.result[0].general[0].storevalue[0].$.average;
+
+    this.storageCostsAc = this.JSONData.results.result[0].general[0].storagecosts[0].$.current;
+    this.storageCostsAv = this.JSONData.results.result[0].general[0].storagecosts[0].$.average;
+
+    this.profitAc = this.JSONData.results.result[0].summary[0].profit[0].$.current;
+    this.profitAv = this.JSONData.results.result[0].summary[0].profit[0].$.average;
+    this.profitAll = this.JSONData.results.result[0].summary[0].profit[0].$.all;
+
+    debugger;
+  }
 }

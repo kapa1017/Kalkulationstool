a4dcd1f8ac69f8e3425af570669097d5b6963199
PaKanzleiter <pascal.kanzleiter@abilis.de>
2017-12-11 22:10:59 +0100

    BackendService in OrderCalculation eingebaut

--- Kalkultationstool/src/app/order-calculation/order-calculation.component.ts
+++ Kalkultationstool/src/app/order-calculation/order-calculation.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {BackendService} from '../Services/backend.service';
+import {OrderService} from '../Services/order.service';
 
 @Component({
   selector: 'app-order-calculation',
   templateUrl: './order-calculation.component.html',
   styleUrls: ['./order-calculation.component.css']
 })
-export class OrderCalculationComponent implements OnInit {
+export class OrderCalculationComponent {
 
-  constructor() { }
+  JSONData: any;
+  orders: any;
 
-  ngOnInit() {
+  constructor(private backendService: BackendService, private orderService: OrderService) {
+    backendService.getData().subscribe((data: Object) => {
+      this.JSONData = data;
+    });
   }
-
 }
